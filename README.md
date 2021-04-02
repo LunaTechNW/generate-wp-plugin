cf36a78a9a725958503bc235506d1aca7831b689
Peter Lama <peterldev94@gmail.com>
2021-04-02 00:58:40 +0000

    First version of action

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # generate-wp-plugin-manifest
+
+A GitHub Action that generates a json file containing update info for a WP Plugin
---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function parsePluginInfoComment(pluginFile) {
+  const pluginFileContent = fs.readFileSync(pluginFile, 'utf8');
+  const pluginInfoBlockRegex = /^<\?php\s*\/\*\s(.*?)\*\//s; // match <?php /*...*/
+  const pluginInfoLineRegex = /^\s\*\s([\w\s]+):\s*(.*)/gm; // match * Some Field: value
+  const regexResult = pluginInfoBlockRegex.exec(pluginFileContent);
+
+  if (!(regexResult && regexResult[1])) {
+      throw new Error(`Failed to parse plugin info comment block in ${pluginFile}`);
+  }
+
+  const parsedPluginInfo = [];
+  let lineMatch = null;
+
+  while (lineMatch = pluginInfoLineRegex.exec(regexResult[1])) {
+    if (lineMatch[1] && lineMatch[2]) {
+      parsedPluginInfo.push({
+        name: lineMatch[1],
+        value: lineMatch[2]
+      });
+    }
+  }
+
+  if (!parsedPluginInfo.length) {
+    throw new Error(`Failed to parse plugin info lines in ${pluginFile}`);
+  }
+
+  const pluginInfo = {};
+  for (const infoField of parsedPluginInfo) {
+    let fieldKey = infoField.name.toLowerCase().replace(/\s/g, '_');
+    pluginInfo[fieldKey] = infoField.value;
+  }
+
+  return pluginInfo;
+}
+
+function run() {
+  try {
+    const outputFile = core.getInput('file');
+    const pluginName = core.getInput('plugin-name');
+    const pluginFile = pluginName + '.php';
+    const pluginInfo = parsePluginInfoComment(pluginFile);
+    const lastCommit = github.context.commits[github.context.commits.length - 1];
+
+    pluginInfo.last_updated = lastCommit.timestamp;
+    pluginInfo.last_commit = github.context.sha;
+
+    const pluginInfoJson = JSON.stringify(pluginInfo, undefined, 2);
+    fs.writeFileSync(outputFile, pluginInfoJson);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
